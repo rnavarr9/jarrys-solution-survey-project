6eaa1f73b8d3e03bb94330abf52598feb400b012
Raj Aseron <raseron@my.centennialcollege.ca>
2021-11-11 17:46:56 -0500

    full crud operations for survey list

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -10,5 +10,20 @@ let surveyController = require('../controllers/survey');
 /* GET Route for the Survey List page - READ Operation */
 router.get('/', surveyController.displayList);
 
+/* GET Route for displaying the Add page - CREATE Operation */
+router.get('/add', surveyController.displayAddPage);
+
+/* POST Route for processing the Add page - CREATE Operation */
+router.post('/add', surveyController.processAddPage);
+
+/* GET Route for displaying the Edit page - UPDATE Operation */
+router.get('/edit/:id', surveyController.displayEditPage);
+
+/* POST Route for processing the Edit page - UPDATE Operation */
+router.post('/edit/:id', surveyController.processEditPage);
+
+/* GET to perform  Deletion - DELETE Operation */
+router.get('/delete/:id', surveyController.performDelete);
+
 
 module.exports= router;
