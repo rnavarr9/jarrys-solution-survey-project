d024f69a3a8a83413938650a07775d8347c3cabd
rnavarr9 <rnavarr9@my.centennialcollege.ca>
2021-11-11 20:02:59 -0500

    Creates npm scripts to deploy client and server

--- server/config/app.js
+++ server/config/app.js
@@ -3,6 +3,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require('cors');
 
 let indexRouter = require('../routes/index');
 let surveyRouter = require('../routes/survey');
@@ -33,10 +34,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../public')));
-app.use(express.static(path.join(__dirname, '../../node_modules')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/node_modules')));
 
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/surveys', surveyRouter);
 
