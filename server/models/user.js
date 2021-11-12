2066752a6feb7955e823348bc6c0c20d89636014
Jay Bhadreshwara <bhadreshwara99@gmail.com>
2021-11-12 12:18:03 -0500

    Modify survey and create a user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,21 +1,25 @@
 // require modules for the User Model
 let mongoose = require("mongoose");
 
-let User = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+let User = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-});
-
+  {
+    collection: "users",
+  }
+);
 module.exports.User = mongoose.model("User", User);
