36e888205049c5ac6ca6cf085dc3158222435e10
Raj Aseron <raseron@my.centennialcollege.ca>
2021-11-11 19:00:44 -0500

    updated comments on the survey routes

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -7,19 +7,19 @@ let survey = require('../models/survey');
 
 let surveyController = require('../controllers/survey');
 
-/* GET Route for the Survey List page - READ Operation */
+/* GET Route for the Survey List - READ Operation */
 router.get('/', surveyController.displayList);
 
-/* GET Route for displaying the Add page - CREATE Operation */
+/* GET Route for displaying the Add survey view- CREATE Operation */
 router.get('/add', surveyController.displayAddSurvey);
 
-/* POST Route for processing the Add page - CREATE Operation */
+/* POST Route for processing the Add survey - CREATE Operation */
 router.post('/add', surveyController.processAddSurvey);
 
-/* GET Route for displaying the Edit page - UPDATE Operation */
+/* GET Route for displaying the Edit survey view - UPDATE Operation */
 router.get('/edit/:id', surveyController.displayEditSurvey);
 
-/* POST Route for processing the Edit page - UPDATE Operation */
+/* POST Route for processing the Edit survey - UPDATE Operation */
 router.post('/edit/:id', surveyController.processEditSurvey);
 
 /* GET to perform  Deletion - DELETE Operation */
