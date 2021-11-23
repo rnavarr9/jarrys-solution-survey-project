a0ebd8e040c18fef319d6455964210d6d02c765f
rnavarr9 <rnavarr9@my.centennialcollege.ca>
2021-11-23 05:33:53 -0500

    Adds path prop to  protected route

--- client/src/Components/ProtectedRoute/index.js
+++ client/src/Components/ProtectedRoute/index.js
@@ -2,10 +2,11 @@ import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import Auth from "../../Contexts/Auth";
 
-const ProtectedRoute = ({ component: Component }) => {
+const ProtectedRoute = ({ component: Component, path }) => {
   const { auth } = useContext(Auth);
   return (
     <Route
+      path={path}
       render={(props) => {
         if (auth) {
           return <Component />;
