d1245b9d6e3ad65fa22a80662c230ba81fa430a0
Raj Aseron <raseron@my.centennialcollege.ca>
2021-12-09 11:20:37 -0500

    backend refactor

--- routes/surveyTemplatesRoutes.js
+++ routes/surveyTemplatesRoutes.js
@@ -2,129 +2,22 @@ const mongoose = require("mongoose");
 const SurveyTemplates = mongoose.model("SurveyTemplates");
 const verifyJWT = require("../middlewares/verifyJWT");
 
+let surveyController = require('../controllers/survey');
+
 module.exports = (app) => {
-  app.get(`/api/survey-templates`, async (req, res) => {
-    try {
-      let surveyTemplates = await SurveyTemplates.find();
-      console.log({surveyTemplates})
-      return res.json(surveyTemplates);
-    } catch (error) {
-      return res.send(error);
-    }
-  });
+  app.get(`/api/survey-templates`, surveyController.getAllSurveyTemplate);
 
-  app.get(`/api/admin/survey-templates`, verifyJWT, async (req, res) => {
-    const userIdInToken = res.locals.id;
-    try {
-      let surveyTemplates = await SurveyTemplates.find({
-        user: { _id: userIdInToken },
-      }).populate("user", "username");
-      // const surveyTemplates = await SurveyTemplates.find().populate('user', 'username').select('type user');
-      return res.json(surveyTemplates);
-    } catch (error) {
-      return res.send(error);
-    }
-  });
+  app.get(`/api/admin/survey-templates`, verifyJWT, surveyController.getAllAdminSurveyTemplate);
 
-  app.get(`/api/admin/survey-templates/:id`, verifyJWT, async (req, res) => {
-    let id = req.params.id;
-    try {
-      const surveyTemplate = await SurveyTemplates.findById(id).exec();
-      return res.json(surveyTemplate);
-    } catch (error) {
-      return res.send(error);
-    }
-  });
+  app.get(`/api/admin/survey-templates/:id`, verifyJWT, surveyController.getSpecificAdminSurveyTemplate);
 
-  app.get(`/api/survey-templates/:id`, async (req, res) => {
-    let id = req.params.id;
-    try {
-      const surveyTemplate = await SurveyTemplates.findById(id).exec();
-      return res.json(surveyTemplate);
-    } catch (error) {
-      return res.send(error);
-    }
-  });
+  app.get(`/api/survey-templates/:id`, surveyController.getSurveyTemplate);
 
-  app.post(`/api/survey-templates/add`, verifyJWT, async (req, res) => {
-    let newSurveyTemplate = SurveyTemplates({
-      title: req.body.title,
-      type: req.body.type,
-      questions: req.body.questions,
-      user: res.locals.id,
-      active: true,
-    });
+  app.post(`/api/survey-templates/add`, verifyJWT, surveyController.addSurveyTemplate);
 
-    SurveyTemplates.create(newSurveyTemplate, (err, surveyTemplate) => {
-      if (err) {
-        console.log(err);
-        res.end(err);
-      } else {
-        console.log("Survey Template created!", surveyTemplate);
-        res.json({ success: true, msg: "New Survey Template added!." });
-      }
-    });
-  });
+  app.get(`/api/survey-templates/delete/:id`, verifyJWT, surveyController.deleteSurveyTemplate);
 
-  app.get(
-    `/api/survey-templates/delete/:id`,
-    verifyJWT,
-    async (req, res, next) => {
-      let id = req.params.id;
-      SurveyTemplates.remove({ _id: id }, (err) => {
-        if (err) {
-          console.log(err);
-          res.end(err);
-        } else {
-          res.json({ success: true, msg: "Survey Template deleted." });
-        }
-      });
-    }
-  );
+  app.get(`/api/survey-templates/update/:id`, verifyJWT, surveyController.getSurveyTemplateForUpdate);
 
-  app.get(
-    `/api/survey-templates/update/:id`,
-    verifyJWT,
-    async (req, res, next) => {
-      let id = req.params.id;
-
-      SurveyTemplates.findById(id, (err, itemToEdit) => {
-        if (err) {
-          console.log(err);
-          res.end(err);
-        } else {
-          res.json({ success: true, msg: "", surveyTemplate: itemToEdit });
-        }
-      });
-    }
-  );
-
-  app.post(
-    `/api/survey-templates/update/:id`,
-    verifyJWT,
-    async (req, res, next) => {
-      let id = req.params.id;
-
-      let updatedSurveyTemplate = SurveyTemplates({
-        _id: id,
-        title: req.body.title,
-        type: req.body.type,
-        questions: req.body.questions,
-        active: req.body.active,
-      });
-
-      SurveyTemplates.updateOne({ _id: id }, updatedSurveyTemplate, (err) => {
-        if (err) {
-          console.log(err);
-          res.end(err);
-        } else {
-          res.json({
-            success: true,
-            msg: "Survey Template updated.",
-            surveyTemplate: updatedSurveyTemplate,
-          });
-        }
-      });
-    }
-  );
+  app.post(`/api/survey-templates/update/:id`, verifyJWT, surveyController.processSurveyTemplateForUpdate );
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,90 +3,18 @@ const Users = mongoose.model("Users");
 const verifyJWT = require("../middlewares/verifyJWT");
 const bcrypt = require("bcrypt");
 
+let userController = require('../controllers/user')
+
 module.exports = (app) => {
-  app.get(`/api/users`, verifyJWT, async (req, res) => {
-    const userId = res.locals.id;
-    try {
-      const users = await Users.findById(userId).select('-password');
-      console.log({users})
-      
-      return res.json([users]);
-    } catch (error) {
-      return res.send(error);
-    }
-  });
+  app.get(`/api/users`, verifyJWT, userController.getAllUsers);
 
-  app.get(`/api/users/:id`, verifyJWT, async (req, res) => {
-    let id = req.params.id;
-    try {
-      const user = await Users.findById(id).select('-password');
-      
-      return res.json(user);
-    } catch (error) {
-      return res.send(error);
-    }
-  });
+  app.get(`/api/users/:id`, verifyJWT, userController.getUser);
 
-  app.post(`/api/users/add`, verifyJWT, async (req, res) => {
-    let newUser = Users({
-      name: req.body.name,
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-      //add bcrypt to password
-    });
-    Users.create(newUser, (err, user) => {
-      if (err) {
-        console.log(err);
-        res.end(err);
-      } else {
-        console.log("User created!", user);
-        res.json({ success: true, msg: "New user added!." });
-      }
-    });
-  });
-  app.get(`/api/users/delete/:id`, verifyJWT, async (req, res, next) => {
-    let id = req.params.id;
-    Users.remove({ _id: id }, (err) => {
-      if (err) {
-        console.log(err);
-        res.end(err);
-      } else {
-        res.json({ success: true, msg: "User deleted." });
-      }
-    });
-  });
-  app.get(`/api/users/update/:id`, verifyJWT, async (req, res, next) => {
-    let id = req.params.id;
-    Users.findById(id, (err, userToEdit) => {
-      if (err) {
-        console.log(err);
-        res.end(err);
-      } else {
-        res.json({ success: true, msg: "", user: userToEdit });
-      }
-    });
-  });
-  app.post(`/api/users/update/:id`, verifyJWT, async (req, res, next) => {
-    let id = req.params.id;
-    let updateUser = Users({
-      _id: id,
-      name: req.body.name,
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    Users.updateOne({ _id: id }, updateUser, (err) => {
-      if (err) {
-        console.log(err);
-        res.end(err);
-      } else {
-        res.json({
-          success: true,
-          msg: "User updated.",
-          user: updateUser,
-        });
-      }
-    });
-  });
+  app.post(`/api/users/add`, verifyJWT, userController.addUser);
+
+  app.get(`/api/users/delete/:id`, verifyJWT, userController.deleteUser );
+
+  app.get(`/api/users/update/:id`, verifyJWT, userController.getUserForUpdate);
+
+  app.post(`/api/users/update/:id`, verifyJWT, userController.processUserForUpdate);
 };
