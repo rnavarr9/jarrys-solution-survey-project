05197f7b60e2c0755be8dc4f175dce310e047e9f
Renzo Navarro <rcnavarrop@gmail.com>
2021-12-11 01:39:54 -0500

    Fixes bug in update user endpoint and comments add user endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -6,25 +6,25 @@ const bcrypt = require("bcrypt");
 module.exports.getAllUsers = async (req, res) => {
   const userId = res.locals.id;
   try {
-    const users = await Users.findById(userId).select('-password');
-    console.log({ users })
+    const users = await Users.findById(userId).select("-password");
+    console.log({ users });
 
     return res.json([users]);
   } catch (error) {
     return res.send(error);
   }
-}
+};
 
 module.exports.getUser = async (req, res) => {
   let id = req.params.id;
   try {
-    const user = await Users.findById(id).select('-password');
+    const user = await Users.findById(id).select("-password");
 
     return res.json(user);
   } catch (error) {
     return res.send(error);
   }
-}
+};
 
 module.exports.addUser = async (req, res) => {
   let newUser = Users({
@@ -43,7 +43,7 @@ module.exports.addUser = async (req, res) => {
       res.json({ success: true, msg: "New user added!." });
     }
   });
-}
+};
 
 module.exports.deleteUser = async (req, res, next) => {
   let id = req.params.id;
@@ -55,7 +55,7 @@ module.exports.deleteUser = async (req, res, next) => {
       res.json({ success: true, msg: "User deleted." });
     }
   });
-}
+};
 
 module.exports.getUserForUpdate = async (req, res, next) => {
   let id = req.params.id;
@@ -67,17 +67,29 @@ module.exports.getUserForUpdate = async (req, res, next) => {
       res.json({ success: true, msg: "", user: userToEdit });
     }
   });
-}
+};
 
 module.exports.processUserForUpdate = async (req, res, next) => {
   let id = req.params.id;
+
+  const saltRounds = 10;
+  const hashedPassword = await new Promise((resolve, reject) => {
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(hash);
+    });
+  });
+
   let updateUser = Users({
     _id: id,
     name: req.body.name,
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
+
   Users.updateOne({ _id: id }, updateUser, (err) => {
     if (err) {
       console.log(err);
@@ -90,4 +102,4 @@ module.exports.processUserForUpdate = async (req, res, next) => {
       });
     }
   });
-}
+};
